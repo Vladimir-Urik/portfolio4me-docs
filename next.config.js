ce76e6f774ec6dcd2991c9ce67a6a68f80ce559f
Vladimir-Urik <gggedrvideos@gmail.com>
2024-07-29 16:20:15 +0200

    Update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  unstable_staticImage: false,
 })
 
 module.exports = withNextra({
-  trailingSlash: true,
   output: 'export',
   images: {
     loader: "custom",
